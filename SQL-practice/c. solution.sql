15933279bea3424222c9b6b338e31abf2802652f
learning <167541969+knowledge-builder@users.noreply.github.com>
2026-05-19 21:37:30 +0800

    Update c. solution.sql

--- SQL-practice/c. solution.sql
+++ SQL-practice/c. solution.sql
@@ -11,3 +11,10 @@ SELECT DISTINCT department_id department,
     ROUND(AVG(salary) OVER(PARTITION BY department_id), 2) as avg_salary
 FROM employees
 ORDER BY department_id ASC
+
+-- Count tasks by status.
+
+SELECT DISTINCT task_status, 
+    COUNT(task_id) OVER(PARTITION BY task_status) task_count
+FROM tasks
+ORDER BY task_status ASC
