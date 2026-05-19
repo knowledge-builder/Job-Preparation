35d0789d3960096320f7d44afaa60250a274385a
learning <167541969+knowledge-builder@users.noreply.github.com>
2026-05-19 21:34:01 +0800

    Update c. solution.sql

--- SQL-practice/c. solution.sql
+++ SQL-practice/c. solution.sql
@@ -4,3 +4,10 @@ SELECT DISTINCT department_id department,
     COUNT(*) OVER(PARTITION BY department_id) as employee_per_dep
 FROM employees
 ORDER BY department_id ASC
+
+-- Find average salary per department.
+
+SELECT DISTINCT department_id department, 
+    ROUND(AVG(salary) OVER(PARTITION BY department_id), 2) as avg_salary
+FROM employees
+ORDER BY department_id ASC
